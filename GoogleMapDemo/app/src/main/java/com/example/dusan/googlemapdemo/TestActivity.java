047f9d9b8e4f35073a8a0f6b63288d5a17e52d6b
Dusan Spasic <dusan.spasic@htecgroup.com>
2017-05-18 16:15:04 +0200

    test branch first commit

---
+++ GoogleMapDemo/app/src/main/java/com/example/dusan/googlemapdemo/TestActivity.java
@@ -0,0 +1,18 @@
+package com.example.dusan.googlemapdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dusanspasic on 18.5.17..
+ */
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_test);
+    }
+}
